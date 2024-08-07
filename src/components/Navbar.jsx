42f061dd6d40554a1c9aa2c65853762872caf6ec
Tunahan Karakaya <bstunahan@gmail.com>
2024-08-07 12:59:58 +0200

    updated logo and favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ function Navbar() {
             onClick={() => handleLinkClick('/')}
             className={`relative px-3 py-2 rounded-full transition-all duration-300 ease-in-out ${location.pathname === '/' ? 'text-gray-800' : ''}`}
           >
-            <span className="relative z-10">⌘</span>
+            <span className="relative z-10">T</span>
           </Link>
           <Link 
             to="/about" 
